53f4b5c8bb9eebde4354055c7150a524706b1444
Shreyas-Babu <116777572+Shreyas-Babu@users.noreply.github.com>
2022-10-28 08:24:56 -0400

    Update TestEmotionDetector.py

--- TestEmotionDetector.py
+++ TestEmotionDetector.py
@@ -1,53 +1,52 @@
-import cv2
+import matplotlib.pyplot as plt
 import numpy as np
-from keras.models import model_from_json
+#import os
+import cv2
+import pathlib 
 
 
-emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+from tensorflow.keras.utils import img_to_array #, load_img
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Input, GlobalAveragePooling2D, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D, Dropout
+from keras.models import Model, Sequential, load_model
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 
-# load json and create model
-json_file = open('model/emotion_model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-emotion_model = model_from_json(loaded_model_json)
 
-# load weights into new model
-emotion_model.load_weights("model/emotion_model.h5")
-print("Loaded model from disk")
+cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
+clf = cv2.CascadeClassifier(str(cascade_path))
+classifier =load_model(r'C:\Users\shrey\Desktop\AI\training\images\trained_model.h5')
 
-# start the webcam feed
-#cap = cv2.VideoCapture(0)
-
-# pass here your video path
-# you may download one from here : https://www.pexels.com/video/three-girls-laughing-5273028/
-cap = cv2.VideoCapture("C:\\JustDoIt\\ML\\Sample_videos\\emotion_sample6.mp4")
+camera = cv2.VideoCapture(0)
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
 while True:
-    # Find haar cascade to draw bounding box around face
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (1280, 720))
-    if not ret:
-        break
-    face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  _, frame = camera.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  faces = clf.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(40, 40), flags= cv2.CASCADE_SCALE_IMAGE)
+  
 
-    # detect faces available on camera
-    num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+  for(x, y, width, height) in faces:
+     cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 255, 0), 2)
+     gray_img = gray[y:y+height, x:x+width]
+     gray_img = cv2.resize(gray_img,(48,48),interpolation=cv2.INTER_AREA)
+     
+     if np.sum([gray_img])!=0:
+            roi = gray_img.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+  
+            prediction = classifier.predict(roi)[0]  
+            label=emotion_labels[prediction.argmax()]
+            label_position = (x,y-30)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+     else:
+            cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    
 
-    # take each face available on the camera and Preprocess it
-    for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
-        roi_gray_frame = gray_frame[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+  cv2.imshow("Faces", frame)
+  if cv2.waitKey(1) == ord("q"):
+       break
 
-        # predict the emotions
-        emotion_prediction = emotion_model.predict(cropped_img)
-        maxindex = int(np.argmax(emotion_prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-    cv2.imshow('Emotion Detection', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
+camera.release()
 cv2.destroyAllWindows()
